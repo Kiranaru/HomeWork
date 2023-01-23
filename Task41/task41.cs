efaa86b5143f80e2a2b2fb3eef34dc6904b4dd7e
Kirill Khvostunkov <65184247+Kiranaru@users.noreply.github.com>
2023-01-23 23:35:00 +0300

    Done
    Done

---
+++ Task41/task41.cs
@@ -0,0 +1,14 @@
+﻿string str = "3,5,6,8,0,-34";
+string[] strArray = str.Split(',');
+
+PositiveNumbersCounter(strArray);
+
+void PositiveNumbersCounter(string[] array)
+{
+    int counter=0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if (Convert.ToInt32(array[i])>0) counter++;
+    }
+    Console.WriteLine($"Number of Positive elements in You Array is: {counter}");
+}
