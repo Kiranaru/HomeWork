1e7c557860b1a684e4ece0c4e49418d4e3117fb2
Kirill Khvostunkov <65184247+Kiranaru@users.noreply.github.com>
2023-02-03 13:33:13 +0300

    Two methods for input and output numers have been created

--- Task64/task64.cs
+++ Task64/task64.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int numMin = 2;
+Console.WriteLine($"Pleade, input positive Number more then {numMin}");
+
+int number = NumeroInput(numMin);
+
+NaturalNumbersOutput(number);
+
+int NumeroInput(int numM)
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+    while (num < numM)
+    {
+        Console.WriteLine("I told you, input positive Number more then 1! Try again:");
+        num = Convert.ToInt32(Console.ReadLine());
+    }
+    return num;
+}
+
+void NaturalNumbersOutput(int num)
+{
+    if (num == 1) { Console.Write($"{num}"); return; }
+    Console.Write($"{num}, ");
+    NaturalNumbersOutput(num - 1);
+}
